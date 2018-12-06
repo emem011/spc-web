3b1f031a7cbbaa9d83e1c6ac2f0dc61b0b498b39
hexccc <hexcordez@gmail.com>
2018-12-06 11:17:24 +0800

    Initialdropdown

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,12 +4,17 @@
   <h1>Home</h1>
   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 @endsection
-
 @section('sidebar')
   <div class="buttonf">
-    <a href="form" type="button" class="btn btn-info">Fill Up Forms</a>
+   <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
+     Fill Up Forms
+   </button>
+   <div class="dropdown-menu">
+     <a class="dropdown-item" href="#">Link 1</a>
+     <a class="dropdown-item" href="#">Link 2</a>
+     <a class="dropdown-item" href="#">Link 3</a>
+   </div>
   </div>
-  
   @parent
   <p>This is appended to the sidebar</p>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,10 @@
     <meta charset="utf-8">
     <title>SPC Website</title>
     <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
   </head>
   <body>
     @include('inc.navbar')
